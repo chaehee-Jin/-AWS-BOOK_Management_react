07688577385bda6b3e4ff9f6426f656aaa32a40c
heehee <heeheer_g@naver.com>
2023-04-17 17:42:13 +0900

    첫커밋, 기본설정

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+           <header>
+                <h1>Login</h1>
+           </header>
+           <main>
+                <div>
+                    <input type="text"></input>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+           </main>
+           <footer>
+                <div></div>
+           </footer>
+
+        </div>
+    );
+};
+
+export default Login;
