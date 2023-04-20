28eb709154ad8c985805f5ef8e0ccf9f5ada7056
heehee <heeheer_g@naver.com>
2023-04-20 17:49:10 +0900

    로그인 토큰

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Callback from './study/Callback';
 import PromiseStudy from './study/PromiseStudy';
+import Main from './pages/Main/Main';
+import AuthRoute from './components/Routes/AuthRoute/AuthRoute';
+
 
 
 function App() {
+
   return (
 <>
     <Global styles={ Reset }></Global>
     <Routes>
-      <Route exact path="/login" Component={Login}></Route>
-      <Route path="/register" Component={Register}></Route>
-      <Route path="/callback" Component={Callback}></Route>
-      <Route path="/promise" Component={PromiseStudy}></Route>
+      <Route exact path="/login" element={<Login/>}></Route>
+      <Route path="/register" element={<Register/>}></Route>
+      
+      <Route path="/" element={
+        <AuthRoute authenticated={false} element={<Main> </Main>}/>
+      }/>
+      {/* <Route path="/callback" Component={Callback}></Route>
+      <Route path="/promise" Component={PromiseStudy}></Route> */}
       
     </Routes >
 </>
